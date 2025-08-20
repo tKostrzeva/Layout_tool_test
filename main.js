48070169a4f0e2a9d09d27cbf872b786b0b3d8c0
tKostrzeva <123314064+tKostrzeva@users.noreply.github.com>
2025-08-20 17:10:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,15 +13,9 @@ let elliPosY = document.getElementById("elliPosY");
 var colorSelectorB = document.getElementById("colorSelectorB");
 let elliGrad = document.getElementById("elliGrad");
 
-let img, imgUp;
-
 let status = false;
 
 let finalPG;
-
-function preload() {
-  img = loadImage("media/02_1_facies_dorsalis_cerebri.jpg");
-}
 
 function setup() {
   var c = createCanvas(windowWidth - 300, windowHeight);
@@ -36,28 +30,10 @@ function setup() {
 
   noStroke();
 
-  /*   let uploadElement = document.getElementById("imageUpload");
-    uploadElement.addEventListener("change", handleImageUpload); */
-  document.querySelector("button").addEventListener("click", switch_status);
-
   imageMode(CENTER);
   rectMode(CENTER);
 }
 
-/* function handleImageUpload(event) {
-  let file = event.target.files[0];
-  if (file) {
-    let reader = new FileReader();
-    reader.onload = function (e) {
-      imgUp = loadImage(e.target.result);
-    };
-    reader.readAsDataURL(file);
-  }
-} */
-
-function switch_status() {
-  status = !status;
-}
 
 function draw() {
   updatePgA();
@@ -70,28 +46,6 @@ function draw() {
     clear();
     status = !status;
   }
-
-
-
-
-  /*   ////////// LAYER A - rectangle
-    push();
-    translate(width / 2, height / 2);
-    image(pgA, 0, 0);
-    pop();
-  
-    ////////// LAYER C - text
-    push();
-    translate(width / 2, height / 2);
-    image(pgC, 0, 0);
-    pop();
-  
-    ////////// LAYER B - ellipse
-    push();
-    translate(width / 2, height / 2);
-    image(pgB, 0, 0);
-    pop();
-     */
 
 
   finalPG.push();
@@ -119,6 +73,5 @@ function hexToRgba(hex, alpha) {
 }
 
 function saveImage() {
-  /* save(); */
   finalPG.save("kompozicia.png");
 }
